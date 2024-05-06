d047407103a464aaf6bc09217bbc8f92ed6d6d5c
MrRevillod <lrevillod2022@alu.uct.cl>
2024-05-06 13:55:09 -0400

    fix: auth middlewares && cors

--- authentication/src/routes/mod.rs
+++ authentication/src/routes/mod.rs
@@ -1,23 +1,49 @@
 
 use std::sync::Arc;
 
-use axum::routing::Router;
+use axum::routing::{get, Router};
 use axum::routing::{post, patch};
-use axum::middleware::from_fn_with_state;
+use axum::middleware::{from_fn, from_fn_with_state};
 
+use bcrypt::hash;
 use lxha_lib::app::state::AppContext;
+
+use axum::extract::State;
+use axum_responses::{AxumResponse, HttpResponse};
+use lxha_lib::app::Context;
+use lxha_lib::models::user::{Role, User};
+use mongodb::bson::oid::ObjectId;
 
 use crate::{
     controllers::account::*,
     controllers::authentication::*,
     middlewares::session::session_validation,
     middlewares::role::protected_role_validation,
+    middlewares::validations::owner_validation,
     middlewares::validations::is_valid_id_and_token,
 };
+
+pub async fn test_controller(State(ctx): Context) -> AxumResponse {
+
+    let user = User {
+        id: ObjectId::new(),
+        username: "Luciano".into(),
+        email: "mail@mail.com".into(),
+        role: Role::ADMINISTRATOR,
+        password: hash("aaa", 7).unwrap(),
+        validated: true,
+        instances: vec![]
+    };
+
+    ctx.users.create(&user).await?;
+    Ok(HttpResponse::OK)
+}
 
 pub fn auth_router(state: Arc<AppContext>) -> Router<Arc<AppContext>> {
 
     Router::new()
+
+        .route("/register-test", get(test_controller))
         
         .route("/login", post(login_controller))
 
@@ -30,12 +56,12 @@ pub fn auth_router(state: Arc<AppContext>) -> Router<Arc<AppContext>> {
         )
         
         .route("/validate-role", post(authenticate)
-            .route_layer(from_fn_with_state(Arc::clone(&state), protected_role_validation))
+            .route_layer(from_fn(protected_role_validation))
             .route_layer(from_fn_with_state(Arc::clone(&state), session_validation))
         )
 
         .route("/validate-owner", post(authenticate)
-            .route_layer(from_fn_with_state(Arc::clone(&state), protected_role_validation))
+            .route_layer(from_fn(owner_validation))
             .route_layer(from_fn_with_state(Arc::clone(&state), session_validation))
         )
 
