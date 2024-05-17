8b5d757f777775da511125b9068b2fc29dc5422e
omellado <omellado2022@alu.uct.cl>
2024-05-17 15:13:30 -0400

    feature: delete instance

--- dashboard/src/routes/instances.rs
+++ dashboard/src/routes/instances.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use axum::routing::{get, post, Router};
+use axum::routing::{get, post, delete, Router};
 use lxha_lib::app::state::AppContext;
 
 use crate::{
@@ -13,5 +13,6 @@ pub fn instances_router(state: Arc<AppContext>) -> Router<Arc<AppContext>> {
         .route("/", get(list_instances_controller))
         .route("/", post(create_instance_controller))
         .route("/:instance_name", get(instance_controller))
+        .route("/:instance_name", delete(delete_instance_controller))
         .with_state(state)
 }
